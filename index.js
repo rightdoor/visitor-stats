8072ba273f51b1963cc2b4d1b704cc3e02fcb82b
rightdoor <785762174@qq.com>
2026-01-03 23:13:50 +0800

    修改单词拼写

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ async function handleStats(request, env) {
 
 async function cleanupOldData(env) {
   // 仅清理visits
-  const ninetyDaysMs = 24 * 60 * 60 * 1000;
-  const cutoffTime = Date.now() - ninetyDaysMs;
+  const setDaysMs = 24 * 60 * 60 * 1000;
+  const cutoffTime = Date.now() - setDaysMs;
   await env.DB.prepare('DELETE FROM visits WHERE visit_time < ?').bind(cutoffTime).run();
 }
